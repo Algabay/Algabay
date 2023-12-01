869eb6978b141813284f10cfe3ce51200122d8a2
Biswarup sen <biswarupsen.work@gmail.com>
2023-12-01 13:31:53 +0530

    Update Prizes.js

--- Components/Prizes.js
+++ Components/Prizes.js
@@ -6,7 +6,7 @@ const Prizes = () => {
       <h3 className="text-center text-2xl pb-6 -mt-2 text-gray-600 max-sm:text-sm">
         Supported By
       </h3>
-      <div className="flex justify-between px-24 ml-2 max-sm:inline-block max-sm:ml-3">
+      <div className="flex justify-between px-24 ml-2 max-sm:inline-block max-sm:ml-5">
         <img src="/google.png" className="h-28 max-sm:h-14 max-sm:ml-3" />
         <img src="/meity.png" className="h-32 -ml-8 max-sm:h-14 max-sm:ml-4" />
         <img src="/microsoft.png" className="h-20 mt-6 max-sm:h-14" />
