2c8154cb783f7bf3c8336d5a4a2c728188e084c7
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-01-21 02:09:57 +0530

    responsiveness added

--- Components/CalculatorsBanner.js
+++ Components/CalculatorsBanner.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Link from "next/link";
+// ... (imports)
 
 const CalculatorCard = ({ href, title, description, imageSrc }) => {
   return (
     <Link href={href}>
-      <div className="w-48 h-48 max-sm:w-32 max-sm:h-32 bg-white rounded-xl flex flex-col items-center justify-center p-4 cursor-pointer hover:shadow-xl transition duration-300 ease-in-out shadow-md m-10 border border-gray-100">
+      <div className="w-48 h-48  bg-white rounded-xl flex flex-col items-center justify-center p-4 cursor-pointer hover:shadow-xl transition duration-300 ease-in-out shadow-md m-10 border border-gray-100">
         <h4 className="text-lg text-gray-700">{title}</h4>
         <p className="text-sm text-center mt-1 text-gray-600">{description}</p>
         <img
           src={imageSrc}
           className="h-16 max-sm:h-10 mt-2"
-          alt={`${title} Calculator Image`}
+          alt={`${title} Calculator`}
         />
       </div>
     </Link>
@@ -19,7 +20,7 @@ const CalculatorCard = ({ href, title, description, imageSrc }) => {
 
 const CalculatorsBanner = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 h-[90vh] justify-center items-center px-4 py-4 max-sm:grid max-sm:grid-cols-2 max-sm:gap-4 rounded-xl">
+    <div className="grid grid-cols-2 gap-8 justify-center items-center max-sm:grid max-sm:grid-cols-1 max-sm:gap-2 rounded-xl">
       <div className="flex flex-col items-center justify-center">
         <CalculatorCard
           href="Interest"
