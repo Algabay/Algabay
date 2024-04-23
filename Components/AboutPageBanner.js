a0e18bcee818d25cb4aad950037cdfd7c941ba3f
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-04-23 12:46:22 +0530

    about page

--- Components/AboutPageBanner.js
+++ Components/AboutPageBanner.js
@@ -134,25 +134,6 @@ const AboutPageBanner = () => {
               </a>
             </p>
           </div>
-
-          <div className="h-auto border border-gray-300 mt-8 justify-center p-6 rounded-md">
-            <p className="max-sm:text-sm">
-              <span className="text-2xl text-gray-800 underline">
-                Our other team members →
-              </span>
-              <br />
-              <br />
-              <span className="text-xl text-gray-600">
-                Akhil Joshua
-                (HR)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;Aman
-                Singh Deo (Data
-                Analyst)&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;Neha
-              </span>
-
-              <br />
-              <br />
-            </p>
-          </div>
         </div>
       </div>
     </>
